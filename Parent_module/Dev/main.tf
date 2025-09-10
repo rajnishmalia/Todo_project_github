1cc0c331450ee92023fe17a834d8ad0d6a555c4b
Rajnish Kumar <rajnishmalia@gmail.com>
2025-09-10 11:22:25 +0530

    new code

--- Parent_module/Dev/main.tf
+++ Parent_module/Dev/main.tf
@@ -1,4 +1,4 @@
-locals{
+locals {
     common_tags = {
         "managed_by" = "terraform"
         "owner" = "TodoAppTeam"
@@ -6,7 +6,7 @@ locals{
     }
 }
 
-module "rg"{
+module "rg" {
     source = "../../module/resource_group"
     rg_name = "rg-dev-todoapp"
     rg_location = "centralindia"
@@ -20,7 +20,7 @@ module "acr" {
     rg_name = "rg-dev-todoapp"
     tags = local.common_tags
 }
-module "sql_server"{
+module "sql_server" {
     source = "../../module/sql_server"
     depends_on = [module.rg]
     sql_server_name = "sql-dev-todoapp"
@@ -30,7 +30,7 @@ module "sql_server"{
     admin_password = "P@ssword1234"
     tags = local.common_tags
 }
-module "sql_db"{
+module "sql_db" {
     source = "../../module/sql_db"
     depends_on = [module.sql_server]
     sql_db_name = "sqldb-dev-todoapp"
@@ -38,7 +38,7 @@ module "sql_db"{
     max_size_db = "2"
     tags = local.common_tags
 }
-module "aks"{
+module "aks" {
     source = "../../module/azure_kubernetes_cluster"
     depends_on = [module.rg]
     aks_name = "aks-dev-todoapp"
